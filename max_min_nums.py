ae93e774335fb8d22623ddcae6bb3846427ae594
Poletaev Nikita <nekpolet13@mail.ru>
2023-02-03 01:41:01 +0300

    Решены 5 и 6

---
+++ max_min_nums.py
@@ -0,0 +1,7 @@
+def max_min(numbers):
+    lst_int_nums = [int(el) for el in numbers.split(' ')]
+    return '%i %i' % (max(lst_int_nums), min(lst_int_nums))
+
+
+print(max_min('1 3 -5 20 -35 -15 4'))
+
