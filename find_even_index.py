f51039ecf74361d6780e1d135bb3849eca50a2e9
Poletaev Nikita <nekpolet13@mail.ru>
2023-02-04 17:17:39 +0300

    Revert "Изменение на ветке second"
    This reverts commit bf1fd1f7e7de0d542d6c5f83e74dbf85a89eed92.

--- find_even_index.py
+++ find_even_index.py
@@ -1,8 +1,8 @@
+
 def find_even_index(arr):
     for i in range(len(arr)):
-        if sum(arr[:i]) == sum(arr[i + 1 :]):
+        if sum(arr[:i]) == sum(arr[i+1:]):
             return i
     return -1
 
-
-print("Функция выдает", find_even_index([0, 10, -10]))
+print('Функция выдает', find_even_index([0, 10, -10]))
